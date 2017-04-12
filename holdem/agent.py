52cf432909c3d33bad3b88afdf98de7fff9f1600
aherz12 <alyssa.herz@gmail.com>
2017-04-11 23:09:30 -0400

    iteration 3

--- holdem/agent.py
+++ holdem/agent.py
@@ -1,4 +1,6 @@
 import random
+import numpy as np
+import math
 
 ################################################################################
 class agent():
@@ -8,6 +10,7 @@ class agent():
         self.card1 = None
         self.card2 = None
         self.stack = 500
+        self.level=3
 
     ##################################################
     def handCards(self, card1, card2):
@@ -33,27 +36,43 @@ class agent():
         #action = input("make your move: ")
 
         ## First Iteration AI ##
-        action = random.sample(["c","f","r"],1)
-        print(action)
-        if (action[0] == "r"):
-            value = random.random() * self.stack
-            print(int(value))
-            action = "r " + str(int(value))
-        return action
+        if self.level==1:
+            action = random.sample(["c","f","r"],1)
+            print(action)
+            if (action[0] == "r"):
+                value = random.random() * self.stack
+                print(int(value))
+                action = "r " + str(int(value))
+            
         ########################
 
         ## Second Iteration AI ##
-        rnd = random.random()
-        if (rnd < 0.1):
-            action = "f"
-        elif (rnd < 0.8):
-            action = "c"
-        else:
-            value = random.random() * self.stack
-            print(int(value))
-            action = "r " + str(int(value))
+        elif self.level==2: 
+            rnd = random.random()
+            if (rnd < 0.1):
+                action = "f"
+            elif (rnd < 0.8):
+                action = "c"
+            else:
+                value = random.random() * self.stack
+                print(int(value))
+                action = "r " + str(int(value))
 
-        return action
+            
+
+        ## Third Iteration AI ##
+        elif self.level==3:
+            chenScore=CalculateChen(self)
+            if chenScore> 9:
+                action = "r" + str(int(random.random() * self.stack))
+            else:
+                action = "f"
+
+        return action  
+
+
+
+
         #########################
 
         
@@ -75,6 +94,88 @@ class agent():
 
         # return action
 
+
+    def CalculateChen(self):
+        board = self.dealer.board
+        listOfCards = [self.card1, self.card2]
+        listOfAllCards
+        score = 0
+        for x in board:
+            if x != None:
+                listOfAllCards.append(x)
+        # step 1: Score your highest card only
+        highCard=0
+        for x in listOfCards:
+            if x.ind_val>score1:
+                highCard=x.ind_val
+        if highCard==14:
+            score+=10
+        elif highCard == 13:
+            score+=8
+        elif highCard == 12:
+            score+=7
+        elif highCard == 11:
+            score+=6
+        else:
+            score+=(highCard/2)
+        score+=score1
+
+        # step 2: Multiply pairs by 2 of one card’s value
+        tempValues=[]
+        for x in listOfCards:
+            tempValues.append(x.ind_val)
+        tempValues.sort()
+        for i in range(1,len(tempValues)):
+           if tempValues[i-1]==tempValues[i]:
+            if tempValues[i]==14:
+                score+=20
+            elif tempValues[i] == 13:
+                score+=16
+            elif tempValues[i] == 12:
+                score+=14
+            elif tempValues[i] == 11:
+                score+=12
+            elif tempValues[i]>5
+                score+=tempValues[i]
+            else:
+                score+=5  
+         #step 3: Add 2 points if cards are suited
+        tempSuits=[]
+        for x in listOfCards:
+            tempSuits.append(x.suit)
+        temp=np.unique(tempSuits)
+        if len(temp)>1:
+            score+=2
+
+        #step 4 Subtract points if their is a gap between the two cards.
+        gap=math.abs(listOfCards[0].ind_val-listOfCards[1].ind_val)
+        if gap<5:
+            score-=gap
+        else:
+            score-=5
+        #step 5 Add 1 point if there is a 0 or 1 card gap and both cards are lower than a Q
+            if gap>1 and listOfCards[0].ind_val<12 and listOfCards[1].ind_val<12:
+                score+=1
+        #step 6 Round half point scores up
+
+       score=round(score)         
+        
+            
+                    
+                      
+            
+        
+
+
+
+
+
+
+
+
+
+
+        
     # Dictionary of possible player actions
     #
     # h: prints out this dictionary as a string
