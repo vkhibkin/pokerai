3cdc180496337b7924ff673f01e1052efbeff10f
aherz12 <alyssa.herz@gmail.com>
2017-04-23 12:04:51 -0400

    added levels 4 and 5 to game.py

--- holdem/game.py
+++ holdem/game.py
@@ -1,6 +1,7 @@
 import os
 from player import player
 from agent import agent
+from agentai import agentai
 class game():
 	players=[]
 	def _init_(self):
@@ -23,26 +24,35 @@ class game():
 				selected= 1
 				return self.players
 			elif (choice==2):
-				level=int(input("Enter Agent Level (1-3) :"))
+				level=int(input("Enter Agent Level (1-5) :"))
 				level1=self.checkLevel(level)
-				self.players.append(agent(1, dealerObj,level1))
+				if (level1==5):
+					self.players.append(agentai(1, dealerObj))
+				else:
+					self.players.append(agent(1, dealerObj,level1))
 				self.players.append(player(2, dealerObj))
 				selected= 1
 				return self.players
 			elif(choice==3):
-				level=int(input("Enter Agent 1 Level (1-3) :"))
+				level=int(input("Enter Agent 1 Level (1-5) :"))
 				level1=self.checkLevel(level)
-				self.players.append(agent(1, dealerObj,level1))
-				level=int(input("Enter Agent 2 Level (1-3) :"))
+				if level1==5:
+					self.players.append(agentai(1, dealerObj))
+				else:
+					self.players.append(agent(1, dealerObj,level1))
+				level=int(input("Enter Agent 2 Level (1-5) :"))
 				level2=self.checkLevel(level)
-				self.players.append(agent(2, dealerObj,level2))
+				if level2==5:
+					self.players.append(agentai(2, dealerObj))
+				else:
+					self.players.append(agent(2, dealerObj,level1))
 				selected= 1
 				return self.players
 			else:
 				print("You Entered the Wrong choice")
 #Checks If the Agent Level entered is in range	
 	def checkLevel(self,level):
-		while(level>3):
-			level=int(input("Level Out of Range, Please Enter Again (1-3) :"))
+		while(level>5):
+			level=int(input("Level Out of Range, Please Enter Again (1-5) :"))
 		return level
 			
